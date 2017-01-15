05b14cb08dba5ec638b522a6332df97b34a5f3e4
Cody Deeran <cody@dhcp-10-134-197-207.uawifi.arizona.edu>
2017-01-15 03:57:09 -0700

    Final Submission for Hack AZ 2017

--- src/com/kitchen/timer/KitchenTimerScheduler.java
+++ src/com/kitchen/timer/KitchenTimerScheduler.java
@@ -1,8 +1,7 @@
 package com.kitchen.timer;
 
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.TimeUnit;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClient;
 import com.amazonaws.services.dynamodbv2.document.DeleteItemOutcome;
@@ -15,101 +14,118 @@ import com.amazonaws.services.simpleemail.model.Destination;
 import com.amazonaws.services.simpleemail.model.Message;
 import com.amazonaws.services.simpleemail.model.SendEmailRequest;
 
-
-
 public class KitchenTimerScheduler {
 
 	private long taskDuration;
-	private final ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
-	
 	private final static String COOKING_TIMER_TABLE = "Cooking_Timer";
 	private final static String CUSTOMER_ID = "Customer_ID";
 	private final static String FOOD_ID = "Food_Item";
-	private final static String DURATION_TIME_IN_MILIS = "DURATION_TIME_IN_MILIS";
 
-	public KitchenTimerScheduler(String taskName, String durationInMilis, String customerId, String userId, String foodId, String foodItem) {
+	public KitchenTimerScheduler(String taskName, String durationInMilis, String customerId, String userId,
+			String foodId, String foodItem) {
 
 		this.taskDuration = Long.parseLong(durationInMilis);
 
 		System.out.println("Timer for " + taskName + " has started.");
 
-		Runnable task = () -> {
+		TimerTask timerTask = new TimerTask() {
 
-			System.out.println("Timer for " + taskName + " has completed.");
+			int counter = 0;
 
+			@Override
+			public void run() {
+				System.out.println("TimerTask executing counter is: " + counter);
+				counter++;// increments the counter
+				final String FROM = "cdeeran@gmail.com"; // Replace with your
+															// "From"
+				// address. This address
+				// must be verified.
+				final String TO = "cdeeran@gmail.com"; // Replace with a "To"
+				// address. If your
+				// account is still in
+				// the
+				// sandbox, this address
+				// must be verified.
+				final String BODY = taskName
+						+ " timer has successfully completed, and has been removed from the database.";
+				final String SUBJECT = taskName + "Timer Finished!";
+
+				// Construct an object to contain the recipient address.
+				Destination destination = new Destination().withToAddresses(new String[] { TO });
+
+				// Create the subject and body of the message.
+				Content subject = new Content().withData(SUBJECT);
+				Content textBody = new Content().withData(BODY);
+				Body body = new Body().withText(textBody);
+
+				// Create a message with the specified subject and body.
+				Message message = new Message().withSubject(subject).withBody(body);
+
+				// Assemble the email.
+				SendEmailRequest request = new SendEmailRequest().withSource(FROM).withDestination(destination)
+						.withMessage(message);
+
+				try {
+					System.out.println("Attempting to send email to: " + TO);
+
+					// Instantiate an Amazon SES client, which will make the
+					// service
+					// call.
+					// The service call requires your AWS credentials.
+					// Because we're not providing an argument when
+					// instantiating the
+					// client, the SDK will attempt to find your AWS credentials
+					// using the default credential provider chain. The first
+					// place the
+					// chain looks for the credentials is in environment
+					// variables
+					// AWS_ACCESS_KEY_ID and AWS_SECRET_KEY.
+					// For more information, see
+					// http://docs.aws.amazon.com/sdk-for-java/v1/developer-guide/credentials.html
+					AmazonSimpleEmailServiceClient client = new AmazonSimpleEmailServiceClient();
+					// Send the email.
+					client.sendEmail(request);
+					System.out.println("Email sent successfully!");
+				} catch (Exception ex) {
+					System.out.println("The email was not sent.");
+					System.out.println("Error message: " + ex.getMessage());
+				}
+
+				System.out.println("Deleteing timer: " + taskName + " from database......");
+
+				deleteTask(userId, foodItem);
+			}
 		};
 
-		scheduler.schedule(task, taskDuration, TimeUnit.MILLISECONDS);
+		Timer timer = new Timer(taskName);// create a new Timer
 
-		final String FROM = "cdeeran@gmail.com"; // Replace with your "From"
-													// address. This address
-													// must be verified.
-		final String TO = "cdeeran@gmail.com"; // Replace with a "To"
-												// address. If your
-												// account is still in
-												// the
-												// sandbox, this address
-												// must be verified.
-		final String BODY = taskName + " timer has successfully completed, and has been removed from the database.";
-		final String SUBJECT = taskName + "Timer Finished!";
+		timer.schedule(timerTask, taskDuration);// this line
+																// starts the
+		// timer at the same
+		// time its executed
 
-		// Construct an object to contain the recipient address.
-		Destination destination = new Destination().withToAddresses(new String[] { TO });
+		//
+		// Runnable task = () -> {
+		//
+		// System.out.println("Timer for " + taskName + " has completed.");
+		//
+		// };
 
-		// Create the subject and body of the message.
-		Content subject = new Content().withData(SUBJECT);
-		Content textBody = new Content().withData(BODY);
-		Body body = new Body().withText(textBody);
-
-		// Create a message with the specified subject and body.
-		Message message = new Message().withSubject(subject).withBody(body);
-
-		// Assemble the email.
-		SendEmailRequest request = new SendEmailRequest().withSource(FROM).withDestination(destination)
-				.withMessage(message);
-
-		try {
-			System.out.println("Attempting to send email to: " + TO);
-
-			// Instantiate an Amazon SES client, which will make the service
-			// call.
-			// The service call requires your AWS credentials.
-			// Because we're not providing an argument when instantiating the
-			// client, the SDK will attempt to find your AWS credentials
-			// using the default credential provider chain. The first place the
-			// chain looks for the credentials is in environment variables
-			// AWS_ACCESS_KEY_ID and AWS_SECRET_KEY.
-			// For more information, see
-			// http://docs.aws.amazon.com/sdk-for-java/v1/developer-guide/credentials.html
-			AmazonSimpleEmailServiceClient client = new AmazonSimpleEmailServiceClient();
-			// Send the email.
-			client.sendEmail(request);
-			System.out.println("Email sent successfully!");
-		} catch (Exception ex) {
-			System.out.println("The email was not sent.");
-			System.out.println("Error message: " + ex.getMessage());
-		}
-		
-		System.out.println("Deleteing timer: " + taskName + " from database......");
-		
-		deleteTask(userId, foodItem);
-		
 	}
-	
-	
-	private void deleteTask(String user, String food){
-		
+
+	private void deleteTask(String user, String food) {
+
 		AmazonDynamoDBClient timerDb = new AmazonDynamoDBClient();
 		DynamoDB dynamoDb = new DynamoDB(timerDb);
 		Table dbTable = dynamoDb.getTable(COOKING_TIMER_TABLE);
 		DeleteItemOutcome itemResults = dbTable.deleteItem(CUSTOMER_ID, user, FOOD_ID, food);
-		
-		if(itemResults.getDeleteItemResult() != null){
+
+		if (itemResults.getDeleteItemResult() != null) {
 			System.out.println("Deletion successfull!");
 		} else {
 			System.out.println("Deletion failed!");
 		}
-		
+
 	}
 
 }
--- src/com/kitchen/timer/KitchenTimerSpeechlet.java
+++ src/com/kitchen/timer/KitchenTimerSpeechlet.java
@@ -19,7 +19,7 @@ import com.amazon.speech.speechlet.SpeechletResponse;
 import com.amazon.speech.ui.PlainTextOutputSpeech;
 import com.amazon.speech.ui.SimpleCard;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClient;
-//import com.amazonaws.services.dynamodbv2.document.DeleteItemOutcome;
+import com.amazonaws.services.dynamodbv2.document.DeleteItemOutcome;
 import com.amazonaws.services.dynamodbv2.document.DynamoDB;
 import com.amazonaws.services.dynamodbv2.document.Item;
 import com.amazonaws.services.dynamodbv2.document.ItemCollection;
@@ -46,11 +46,9 @@ public class KitchenTimerSpeechlet implements Speechlet {
 					session.getUser().getUserId());
 		} else if ("Query".equals(intentName)) {
 			return queryDbForTimers(session.getUser().getUserId());
-		}
-//		else if ("Remove".equals(intentName)) {
-//			return deleteTimer(session.getUser().getUserId(), intent.getSlot("Food"));
-//		} 
-		else {
+		} else if ("Delete".equals(intentName)) {
+			return deleteTimer(session.getUser().getUserId(), intent.getSlot("Food"));
+		} else {
 			throw new SpeechletException("Invalid Intent");
 		}
 	}
@@ -156,49 +154,48 @@ public class KitchenTimerSpeechlet implements Speechlet {
 		}
 	}
 
-//	private SpeechletResponse deleteTimer(String userId, Slot foodItem) {
-//		
-//		final String CUSTOMER_ID = "Customer_ID";
-//		final String FOOD_ID = "Food_Item";
-//		final String COOKING_TIMER_TABLE = "Cooking_Timer";
-//		
-//		AmazonDynamoDBClient timerDb = new AmazonDynamoDBClient();
-//		DynamoDB dynamoDb = new DynamoDB(timerDb);
-//		Table dbTable = dynamoDb.getTable(COOKING_TIMER_TABLE);
-//		DeleteItemOutcome itemResults = dbTable.deleteItem(CUSTOMER_ID, userId, FOOD_ID, foodItem);
-//		
-//		if(itemResults.getDeleteItemResult() != null ){
-//			
-//			String timerCouldBeDeleted = foodItem + " timer was successfully deleted.";
-//
-//			// Create the Simple card content.
-//			SimpleCard card = new SimpleCard();
-//			card.setTitle("No Timers Running");
-//			card.setContent(timerCouldBeDeleted);
-//
-//			// Create the plain text output.
-//			PlainTextOutputSpeech speech = new PlainTextOutputSpeech();
-//			speech.setText(timerCouldBeDeleted);
-//
-//			return SpeechletResponse.newTellResponse(speech);
-//		
-//		} else {
-//			
-//			String timerCouldNotBeDeleted = "Sorry, that timer you requested does not exist. Please try again.";
-//
-//			// Create the Simple card content.
-//			SimpleCard card = new SimpleCard();
-//			card.setTitle("No Timers Running");
-//			card.setContent(timerCouldNotBeDeleted);
-//
-//			// Create the plain text output.
-//			PlainTextOutputSpeech speech = new PlainTextOutputSpeech();
-//			speech.setText(timerCouldNotBeDeleted);
-//
-//			return SpeechletResponse.newTellResponse(speech);
-//		}
-//		
-//	}
+	private SpeechletResponse deleteTimer(String userId, Slot foodItem) {
+
+		final String CUSTOMER_ID = "Customer_ID";
+		final String FOOD_ID = "Food_Item";
+		final String COOKING_TIMER_TABLE = "Cooking_Timer";
+
+		AmazonDynamoDBClient timerDb = new AmazonDynamoDBClient();
+		DynamoDB dynamoDb = new DynamoDB(timerDb);
+		Table dbTable = dynamoDb.getTable(COOKING_TIMER_TABLE);
+		DeleteItemOutcome deleteResults = dbTable.deleteItem(CUSTOMER_ID, userId, FOOD_ID, foodItem);
+
+		if (deleteResults.getDeleteItemResult() != null) {
+			String timerCouldBeDeleted = foodItem + " timer was successfully deleted.";
+
+			// Create the Simple card content.
+			SimpleCard card = new SimpleCard();
+			card.setTitle("Timer deleted");
+			card.setContent(timerCouldBeDeleted);
+
+			// Create the plain text output.
+			PlainTextOutputSpeech speech = new PlainTextOutputSpeech();
+			speech.setText(timerCouldBeDeleted);
+
+			return SpeechletResponse.newTellResponse(speech);
+
+		} else {
+
+			String timerCouldBeDeleted = "Sorry, there was no timer to be deleted";
+
+			// Create the Simple card content.
+			SimpleCard card = new SimpleCard();
+			card.setTitle("No Timers Running");
+			card.setContent(timerCouldBeDeleted);
+
+			// Create the plain text output.
+			PlainTextOutputSpeech speech = new PlainTextOutputSpeech();
+			speech.setText(timerCouldBeDeleted);
+
+			return SpeechletResponse.newTellResponse(speech);
+
+		}
+	}
 
 	@Override
 	public SpeechletResponse onLaunch(LaunchRequest arg0, Session arg1) throws SpeechletException {
